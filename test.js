3fe304c88ebc029dda24855ee697adc06c8f9bd2
Jesse_Bogaart <jesseb99@live.nl>
2024-02-07 08:49:55 +0100

    calorie counter

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-var height = process.argv[2];
-var weight = process.argv[3];
-
-var bmi = calculate(height, weight)
 
 
+var height = parseFloat(process.argv[2]);
+var weight = parseFloat(process.argv[3]);
+var age = parseFloat(process.argv[4]);
+var sex = process.argv[5].toLowerCase();
 
-
-function calculate(height, weight) {
-
-    var bmi = weight / (height * height);
-    return bmi;
-
+if (sex === 'men') {
+    let BMRMen = 88.362 + (13.7 * weight) + (5 * height) - (6.8 * age);
+    console.log(`Als man zou je ongeveer ` + BMRMen + ` moeten innemen.`);
+} else if (sex === 'women') {
+    let BMRWomen = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+    console.log(`Als vrouw zou je ongeveer ` + BMRWomen + ` moeten innemen.`);
+} else {
+    console.log("Invalid input for sex. Please provide 'men' or 'women'.");
 }
-
-console.log(bmi)
