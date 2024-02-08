06d6294f928c38f7a9347648b443994a4088c585
Jesse_Bogaart <jesseb99@live.nl>
2024-02-08 08:54:15 +0100

    hello

---
+++ script/script.js
@@ -0,0 +1,34 @@
+//make sure the script is loaded after the dom content is loaded so that the javascript is working properly
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('bmiCalculatorForm').addEventListener('submit', function (e) {
+        e.preventDefault();
+        var height = parseFloat(document.getElementById('height').value);
+        var weight = parseFloat(document.getElementById('weight').value);
+
+        var bmi = calculateBMI(height, weight);
+
+        var bmiResultElement = document.getElementById('bmiResult');
+        bmiResultElement.innerText = "Your BMI is: " + bmi.toFixed(2);
+
+        var bmiResultContainer = document.getElementById('bmiResultContainer');
+        if (bmi < 18.5) {
+            bmiResultContainer.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+            bmiResultContainer.style.borderColor = 'red';
+        } if (bmi > 18.5 && bmi < 25) {
+            bmiResultContainer.style.backgroundColor = 'green';
+            bmiResultContainer.style.borderColor = 'darkgreen';
+        } if (bmi > 25) {
+            bmiResultContainer.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+            bmiResultContainer.style.borderColor = 'red';
+        } else {
+            bmiResultContainer.style.backgroundColor = 'none';
+            bmiResultContainer.style.border = 'none';
+        }
+    });
+    
+
+    function calculateBMI(height, weight) {
+        var bmi = weight / (height * height);
+        return bmi;
+    }
+});
